3c5b292ec6b08ecbc7c9e556fab0dfec28d787a7
phamphukiet <131220958+phamphukiet@users.noreply.github.com>
2024-01-04 07:50:35 +0700

    0.0.0

---
+++ app/lib/appbar/appbar.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import '../appbar/style_appbar.dart';
+
+class MyAppBar extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+      title: Text('MyAppBar', style: lol),
+    );
+  }
+}
---
+++ app/lib/main.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+import 'package:untitled/appbar/appbar.dart';
+import 'package:untitled/navigation/bottom.dart';
+import 'home.dart';
+import 'notifications.dart';
+import 'messages.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int currentPageIndex = 0;
+
+  final pages = [
+    HomePage(),
+    NotificationsPage(),
+    MessagesPage(),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: MyAppBar()),
+      body: pages[currentPageIndex],
+      bottomNavigationBar: MyBottomNavigationBar(
+        onTap: (int index) {
+          setState(() {
+            currentPageIndex = index;
+          });
+        },
+        currentIndex: currentPageIndex,
+      ),
+    );
+  }
+}
