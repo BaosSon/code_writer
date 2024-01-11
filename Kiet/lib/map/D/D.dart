6634ae0f14e76aed4e4297920ba59f8178cd1fa1
phamphukiet <131220958+phamphukiet@users.noreply.github.com>
2024-01-11 09:30:56 +0700

    map 0.0.1

---
+++ Kiet/lib/map/D/D.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class D extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(child: Text('A1'));
+  }
+}
---
+++ Kiet/lib/map/F/F2.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class F2 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(child: Text('A1'));
+  }
+}
