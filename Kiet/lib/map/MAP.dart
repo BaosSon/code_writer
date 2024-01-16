2896ae2087f39f2035528be2f2c6bcc4873cf95c
phamphukiet <131220958+phamphukiet@users.noreply.github.com>
2024-01-16 15:02:46 +0700

    lỗi thôi, đừng bận tâm

--- Kiet/lib/map/MAP.dart
+++ Kiet/lib/map/MAP.dart
@@ -6,7 +6,16 @@ import 'place.dart';
 class Map extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return ThemeText(text: 'Map');
-    // return image.asset('');
+    return Expanded(
+      child:InteractiveViewer(
+      boundaryMargin: EdgeInsets.all(0),
+      minScale: 0.1,
+      maxScale: 10,
+      child: FittedBox(
+          child: Image.asset('image/map.jpg'),
+          fit: BoxFit.cover,
+        ),
+    ));
   }
 }
+
